f331625c2b47f3cf4d5600177392810c7f1456e4
Cydeo-Tetiana <dyopka@gmail.com>
2022-11-14 21:49:56 -0600

    tl tag is added

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.201.219.2059";
+                        String gridAddress = "54.173.119.1";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
